65099a0c94869fd17ea4bec33d425658ccbd6689
Jowan <jowan@ahlewei.com>
2018-06-03 00:18:46 +0800

    Add switch config

--- config/sms.php
+++ config/sms.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'enabled' => true,
     'default' => 'aliyun',
 
     'gateways' => [
--- src/Sms.php
+++ src/Sms.php
@@ -8,10 +8,19 @@ class Sms implements Sender
      * Send Message
      * @param $phone
      * @param $args
+     * @return mixed
      * @throws \Exception
      */
     public function send($phone, $args)
     {
+        $enabled = cache('SMS')['enabled'] ?? config('sms.enabled');
+
+        if (!$enabled) {
+            $error_message = 'SMS service is disabled';
+
+            return request()->wantsJson() ? error($error_message) : show_error($error_message)->send();
+        }
+
         $gateway = cache('SMS')['default'] ?? config('sms.default');
         $this->getGateway($gateway)->send($phone, $args);
     }
